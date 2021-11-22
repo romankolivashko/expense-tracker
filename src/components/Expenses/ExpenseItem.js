a8b23005dbff8f9427ad89cf57c76b6dd60fd5b1
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-22 12:55:55 -0800

    Added onClick function

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,11 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
-import Card from './Card';
+import Card from '../UI/Card';
 
 function ExpenseItem(props){
-
+  const clickHandler = () => {
+    console.log('Clicked!!!');
+  };
   return(
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -11,6 +13,7 @@ function ExpenseItem(props){
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
