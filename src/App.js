64d55bdb02f4d666584c7ca61743e6080e621e11
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-23 09:52:05 -0800

    Added dynamic list functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
-const App = () => {
-  const expenses = [
+
+  const DUMMY_DATA = [
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -26,9 +26,14 @@ const App = () => {
     },
   ];
 
+  const App = () => {
+    const [expenses, setExpenses] = useState(DUMMY_DATA);
+  
+
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];   
+     });
   };
 
   // return React.createElement(
