5a543a4c797c75327d48626a5b971935adcf9096
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-23 22:23:19 -0800

    Added chart styling

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -2,18 +2,24 @@ import React from 'react';
 
 import './ChartBar.css';
 
-const ChartBar = props => {
+const ChartBar = (props) => {
   let barFillHeight = '0%';
-  if (props.max > 0) {
+
+  if (props.maxValue > 0) {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
   }
 
-  return <div className="chart-bar">
-    <div className="chart-bar__inner">
-      <div className="cart-bar__fill" style={{height: barFillHeight }}></div>
+  return (
+    <div className='chart-bar'>
+      <div className='chart-bar__inner'>
+        <div
+          className='chart-bar__fill'
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className='chart-bar__label'>{props.label}</div>
     </div>
-    <div className='chart-bar'>{props.label}</div>
-  </div>
+  );
 };
 
 export default ChartBar;
